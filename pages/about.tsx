4214342c1338f8254515a7b0ba0a83bd115c5a64
William Edmisten <wcedmisten@gmail.com>
2022-06-01 18:58:56 -0400

    Fix responsiveness

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ function AboutPage() {
         <Container>
             <Row><Col><h1>About</h1></Col></Row>
             <Row>
-                <Col>
+                <Col xs={{ span: 12 }} md={{ span: 6 }}>
                     <p>I'm William Edmisten, a software engineer working
                         for Reify Health. I graduated from Virginia Tech in 2019
                         with a BS in Computer Science.</p>
@@ -15,7 +15,7 @@ function AboutPage() {
                         hope to document my progress in a way that might be helpful
                         for others.</p>
                 </Col>
-                <Col>
+                <Col xs={{ span: 12 }} md={{ span: 6 }}>
                     <Image
                         src="william.jpg"
                         rounded={true}
