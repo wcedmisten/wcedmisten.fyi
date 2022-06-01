60a1f3ea8d717bf63184a48ef978a43e78d4cbdd
William Edmisten <wcedmisten@gmail.com>
2022-06-01 18:51:27 -0400

    Add picture of myself to blog

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,30 @@
-import About from '../mdx/about.mdx'
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 function AboutPage() {
-    return <About />
+    return (<>
+        <Container>
+            <Row><Col><h1>About</h1></Col></Row>
+            <Row>
+                <Col>
+                    <p>I'm William Edmisten, a software engineer working
+                        for Reify Health. I graduated from Virginia Tech in 2019
+                        with a BS in Computer Science.</p>
+
+                    <p>I like starting hobby projects, and I'm hoping this blog
+                        will give me more accountability to complete them. I also
+                        hope to document my progress in a way that might be helpful
+                        for others.</p>
+                </Col>
+                <Col>
+                    <Image
+                        src="william.jpg"
+                        rounded={true}
+                        fluid={true}
+                        alt="William pointing at a lime tree at Lewis Ginter Botanical Gardens" />
+                </Col>
+            </Row>
+        </Container>
+    </>)
 }
 
 export default AboutPage
