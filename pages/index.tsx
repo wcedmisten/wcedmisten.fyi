d06ac41d86a2f4b03149456b50bdf6cd43af1371
William Edmisten <wcedmisten@gmail.com>
2022-06-25 21:38:56 -0400

    Add link to new post

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,13 @@ const Home: NextPage = () => {
       <main>
         <h1>Posts</h1>
         <ListGroup variant="flush">
+        <PostItem
+            href="/post/dashcam-to-speed-limits"
+            thumbnailURL="/thumbnails/dashcam-to-speed-limits.jpg"
+            thumbnailAlt="Dashcam image containing speed limit sign"
+            title="Find Missing Speed Limits in Openstreetmap from Dashcam Footage"
+            description='Find Missing Speed Limits using a pipeline with OCR and PostGIS'
+            date='June 25, 2022' />
           <PostItem
             href="/post/dashcam-to-openstreetmap"
             thumbnailURL="/thumbnails/dashcam-to-openstreetmap.png"
