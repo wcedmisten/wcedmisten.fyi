ea0cb678d6c1524e2a6e436b48a2d5c92474b237
William Edmisten <wcedmisten@gmail.com>
2022-08-27 16:01:50 -0400

    Improve painting guesser

--- pages/project/paintingGuesser.tsx
+++ pages/project/paintingGuesser.tsx
@@ -2,6 +2,7 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import { useEffect, useState } from 'react';
 
+import style from "./paintingguesser.module.css"
 
 export const Guesser = (props: { filenames: string[] }) => {
     const { filenames } = props;
@@ -14,11 +15,51 @@ export const Guesser = (props: { filenames: string[] }) => {
 
     const item = index ? filenames[index] : "";
 
-    return (
+    const [selectedArtist, setSelectedArtist] = useState(""); //default value
+
+    function handleArtistSelectChange(event: any) {
+        setSelectedArtist(event.target.value);
+    }
+
+    const [selectedSubject, setSelectedSubject] = useState(""); //default value
+
+    function handleSubjectSelectChange(event: any) {
+        setSelectedSubject(event.target.value);
+    }
+
+    const [selectedDescription, setSelectedDescription] = useState(""); //default value
+
+    function handleDescriptionSelectChange(event: any) {
+        setSelectedDescription(event.target.value);
+    }
+
+    return index !== undefined && (
         <>
-            <h1>Painting Guesser</h1>
-            <h2>Can you guess the descriptions of these famous paintings?</h2>
-            <img src={`/projects/painting-guesser/${item}/00002.jpg`}></img>
+            <h1 className={style.Headers}>Art Gallery Curator</h1>
+            <h2 className={style.Headers}>Oh no! The plaques for these famous paintings got stolen!</h2>
+            <h3 className={style.Headers}>Can you fix them?</h3>
+            <div className={style.Frame}>
+                <img className={style.PaintingImage} src={`/projects/painting-guesser/${item}/00002.jpg`}></img>
+            </div>
+            <div className={style.Plaque}>
+                <p className={style.PlaqueText}>
+                    <select value={selectedArtist} onChange={handleArtistSelectChange} name="artist">
+                        <option value="" disabled>Artist</option>
+                        <option value="Salvador Dali">Salvador Dali</option>
+                    </select> </p>
+                <p className={style.PlaqueText}>
+                    Painting of a {' '}
+                    <select value={selectedSubject} onChange={handleSubjectSelectChange} name="subject" id="subject">
+                        <option value="" disabled>Subject</option>
+                        <option value="Dolphin">Dolphin</option>
+                    </select>
+                    {' '}
+                    <select value={selectedDescription} onChange={handleDescriptionSelectChange} name="description" id="description">
+                        <option value="" disabled>Description</option>
+                        <option value="saab">Wearing a Tophat</option>
+                    </select>
+                </p>
+            </div>
         </>
     )
 }
