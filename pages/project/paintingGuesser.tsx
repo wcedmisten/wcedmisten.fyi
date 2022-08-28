e8439955705a2cb7bf56246358aa0e4fab472d55
William Edmisten <wcedmisten@gmail.com>
2022-08-27 21:37:24 -0400

    update guesser

--- pages/project/paintingGuesser.tsx
+++ pages/project/paintingGuesser.tsx
@@ -3,17 +3,20 @@ import { promises as fs } from 'fs';
 import { useEffect, useState } from 'react';
 
 import style from "./paintingguesser.module.css"
+import { Button } from 'react-bootstrap';
 
 interface GuesserProps {
+    solutions: any,
     filenames: string[];
     artists: string[];
     subjects: string[];
     descriptions: string[];
 }
 
-export const Guesser = (props: GuesserProps) => {
-    const { filenames, artists, subjects, descriptions } = props;
+type GameStatus = "guessing" | "correct" | "wrong";
 
+export const Guesser = (props: GuesserProps) => {
+    const { filenames, artists, subjects, descriptions, solutions } = props;
 
     const [index, setRandomNumber] = useState<number | undefined>(undefined);
 
@@ -21,7 +24,7 @@ export const Guesser = (props: GuesserProps) => {
         setRandomNumber(Math.floor(Math.random() * filenames.length));
     }, []);
 
-    const item = index !== undefined ? filenames[index] : "";
+    const pictureId = index !== undefined ? filenames[index] : "";
 
     const [selectedArtist, setSelectedArtist] = useState(""); //default value
 
@@ -41,13 +44,33 @@ export const Guesser = (props: GuesserProps) => {
         setSelectedDescription(event.target.value);
     }
 
-    const [answerSubmitted, setAnswerSubmitted] = useState<boolean>(false);
     function submitAnswers(event: any) {
-        setAnswerSubmitted(true);
+        if (answersCorrect) {
+            setGameStatus("correct");
+        } else {
+            setGameStatus("wrong");
+        }
     }
 
+    const solution = solutions[pictureId];
+    const answersCorrect: boolean = selectedArtist === solution?.artist &&
+        selectedSubject === solution?.subject &&
+        selectedDescription === solution?.description;
 
     const showSubmitButton: boolean = selectedArtist !== "" && selectedSubject !== "" && selectedDescription !== "";
+
+    const [gameStatus, setGameStatus] = useState<GameStatus>("guessing");
+    const buttonClass = {
+        "guessing": "btn-primary",
+        "correct": "btn-success",
+        "wrong": "btn-danger"
+    }[gameStatus]
+
+    const buttonText = {
+        "guessing": "Engrave Plaque",
+        "correct": "Correct!",
+        "wrong": "Wrong!",
+    }[gameStatus]
 
     return index !== undefined && (
         <>
@@ -55,35 +78,44 @@ export const Guesser = (props: GuesserProps) => {
             <h2 className={style.Headers}>Oh no! The plaques for these famous paintings got stolen!</h2>
             <h3 className={style.Headers}>Can you fix them?</h3>
             <div className={style.Frame}>
-                <img className={style.PaintingImage} src={`/projects/painting-guesser/${item}/00002.jpg`}></img>
+                <img className={style.PaintingImage} src={`/projects/painting-guesser/${pictureId}/00002.jpg`}></img>
             </div>
             <div className={style.Plaque}>
                 <p className={style.PlaqueText}>
-                    <select value={selectedArtist} onChange={handleArtistSelectChange} name="artist">
-                        <option value="" disabled>Artist</option>
-                        {artists.map((artist: string) => {
-                            return <option value={artist}>{artist}</option>
-                        })}
-                    </select> </p>
+                    {gameStatus === "correct" ?
+                        <p className={style.PlaqueText}>{solution.artist}</p> :
+                        <select value={selectedArtist} onChange={handleArtistSelectChange} name="artist">
+                            <option key="placeholder" value="" disabled>Artist</option>
+                            {artists.map((artist: string) => {
+                                return <option key={artist} value={artist}>{artist}</option>
+                            })}
+                        </select>
+                    }
+                </p>
                 <p className={style.PlaqueText}>
                     Painting of a {' '}
-                    <select value={selectedSubject} onChange={handleSubjectSelectChange} name="subject" id="subject">
-                        <option value="" disabled>Subject</option>
-                        {subjects.map((subject: string) => {
-                            return <option value={subject}>{subject}</option>
-                        })}
-                    </select>
+                    {gameStatus === "correct" ?
+                        solution.subject :
+                        <select value={selectedSubject} onChange={handleSubjectSelectChange} name="subject" id="subject">
+                            <option key="placeholder" value="" disabled>Subject</option>
+                            {subjects.map((subject: string) => {
+                                return <option key={subject} value={subject}>{subject}</option>
+                            })}
+                        </select>
+                    }
                     {' '}
-                    <select value={selectedDescription} onChange={handleDescriptionSelectChange} name="description" id="description">
-                        <option value="" disabled>Description</option>
-                        {descriptions.map((description: string) => {
-                            return <option value={description}>{description}</option>
-                        })}
-                    </select>
+                    {gameStatus === "correct" ?
+                        solution.description :
+                        <select value={selectedDescription} onChange={handleDescriptionSelectChange} name="description" id="description">
+                            <option key="placeholder" value="" disabled>Description</option>
+                            {descriptions.map((description: string) => {
+                                return <option key={description} value={description}>{description}</option>
+                            })}
+                        </select>}
                 </p>
             </div>
             {showSubmitButton &&
-                <button onClick={submitAnswers}>Engrave Plaque</button>}
+                <Button className={buttonClass} onClick={submitAnswers}>{buttonText}</Button>}
         </>
     )
 }
@@ -101,9 +133,14 @@ export async function getStaticProps() {
     const subjects = new Set<string>();
     const descriptions = new Set<string>();
 
+    const solutions: any = {
+
+    }
+
     for (const filename of filenames) {
         const filePath = path.join(postsDirectory, filename)
-        const fileContents = await fs.readFile(`${filePath}/prompts.txt`,
+        const fileContents = await fs.readFile(
+            `${filePath}/prompts.txt`,
             'utf8')
 
         const artist = fileContents.split("painting by ")[1].split(" of a ")[0];
@@ -114,11 +151,16 @@ export async function getStaticProps() {
         artists.add(artist);
         subjects.add(subject);
         descriptions.add(description);
+
+        solutions[filename] = {
+            artist, subject, description
+        }
     };
 
     return {
         props: {
             filenames,
+            solutions,
             artists: Array.from(artists),
             subjects: Array.from(subjects),
             descriptions: Array.from(descriptions)
