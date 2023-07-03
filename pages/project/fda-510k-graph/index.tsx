97c264566e6fcd24022fdcd14f5b79915d7fd4a0
William Edmisten <wcedmisten@gmail.com>
2023-07-02 21:08:40 -0400

    Get graph working a bit more

--- pages/project/fda-510k-graph/index.tsx
+++ pages/project/fda-510k-graph/index.tsx
@@ -1,52 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import dynamic from 'next/dynamic';
 
 import style from './fda-510k.module.css'
 
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
-function InfoModal() {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    return (
-        <>
-            <Button variant="secondary" className="d-flex justify-content-center btn-sm" onClick={handleShow}>
-                See more info
-            </Button>
-
-            <Modal show={show} onHide={handleClose} >
-                <Modal.Header closeButton>
-                    <Modal.Title>Welcome to FoodGraph</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>This project is based on a dataset of 91,000
-                        recipes from allrecipes.com
-                    </p>
-                    <p>For more information on how this graph was
-                        created, see{' '}
-                        <a href="https://github.com/wcedmisten/foodFinder" target="_blank">my github repository</a>.
-                    </p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
-}
+import { useWindowSize } from "@react-hook/window-size";
 
 const ForceGraph = dynamic(() => import('../../../components/ForceGraph'), {
     ssr: false,
 })
 
-const SearchInput = ({onInputChange}: {onInputChange: (e: any) => void}) => {
+const SearchInput = ({ onInputChange }: { onInputChange: (e: any) => void }) => {
     const [searchInput, setSearchInput] = useState("")
 
     useEffect(() => {
@@ -55,14 +19,14 @@ const SearchInput = ({onInputChange}: {onInputChange: (e: any) => void}) => {
                 onInputChange(searchInput)
             }
         }, 2000)
-    
-        return () => clearTimeout(getData)
-      }, [searchInput])
 
-    return <input className={style.SearchInput} placeholder="Search for a device" onChange={e => setSearchInput(e.target.value)}/>
+        return () => clearTimeout(getData)
+    }, [searchInput])
+
+    return <input className={style.SearchInput} placeholder="Search for a device" onChange={e => setSearchInput(e.target.value)} />
 }
 
-export const FoodGraph = () => {
+export const DeviceGraph = () => {
     const [graphData, setGraphData] = useState<any>({ links: [], nodes: [] });
 
     const [selectedNode, setSelectedNode] = useState<string>("K223649");
@@ -84,6 +48,8 @@ export const FoodGraph = () => {
     const findNode = (nodeId: string) => {
         return graphData.nodes.find((node: { id: string }) => node.id === nodeId)
     }
+
+    const selectedNodeData = graphData.length ? null : findNode(selectedNode)
 
     const findPredicates = (nodeId: string) => {
         return graphData.links.filter((link: { source: string; target: string }) => link.target === nodeId)
@@ -124,8 +90,19 @@ export const FoodGraph = () => {
 
     const finalData = findSubgraph(graphData, selectedNode);
 
+    const [width, height] = useWindowSize()
+
     return <>
         <SearchInput onInputChange={setSelectedNode}></SearchInput>
+        <div className={style.InfoSection}>
+            <span>Device ID: {selectedNode} </span>
+            <span>Date Recieved: {selectedNodeData?.date} </span>
+            <span>Product Code: <a href={
+                `https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfPCD/classification.cfm?id=${selectedNodeData?.product_code}`
+            }>{selectedNodeData?.product_code}</a> </span>
+            <span>Generic Name: {graphData?.product_descriptions?.[selectedNodeData?.product_code]}</span>
+        </div>
+
         <ForceGraph
             graphData={finalData}
             nodeLabel={node => `ID: ${node.id}<br>Date: ${node.date}<br>Category: ${node.product_code}`}
@@ -133,15 +110,17 @@ export const FoodGraph = () => {
             linkDirectionalArrowLength={3.5}
             linkDirectionalArrowRelPos={1}
             dagMode="radialin"
-            dagLevelDistance={30}
+            width={width - 50}
+            height={height - 200}
+            dagLevelDistance={20}
             nodeVal={(node: any) => node.id === selectedNode ? 10 : 1}
             nodeCanvasObject={(node, ctx, globalScale) => {
                 const label = node.id;
-                const fontSize = 12/globalScale;
+                const fontSize = 12 / globalScale;
                 ctx.font = `${fontSize}px Sans-Serif`;
                 const textWidth = ctx.measureText(label).width;
                 const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
-    
+
                 ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
 
                 ctx.beginPath();
@@ -154,23 +133,23 @@ export const FoodGraph = () => {
                     ctx.strokeStyle = '#003300';
                     ctx.stroke();
                 }
-    
+
                 node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
-              }}
+            }}
         />
     </>
 };
 
-export default FoodGraph
+export default DeviceGraph
 
 export async function getStaticProps() {
     return {
         props: {
             opengraph: {
-                title: "All Recipes Graph Visualization",
-                image: "https://wcedmisten.fyi/og-images/recipe-graph-full.png",
+                title: "FDA 510k Approval Graph",
+                image: "https://wcedmisten.fyi/og-images/fda-510k-graph.png",
                 type: "website",
-                url: "https://wcedmisten.fyi/project/foodGraph/"
+                url: "https://wcedmisten.fyi/project/fda-510k-graph/"
             }
         }
     }
