8c46d3728297d81ef4e11813698c368fad405e1f
William Edmisten <wcedmisten@gmail.com>
2023-07-06 07:25:33 -0400

    Update visualization

--- pages/project/fda-510k-graph/index.tsx
+++ pages/project/fda-510k-graph/index.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import dynamic from 'next/dynamic';
 
 import style from './fda-510k.module.css'
-
-import { useWindowSize } from "@react-hook/window-size";
 
 const ForceGraph = dynamic(() => import('../../../components/ForceGraph'), {
     ssr: false,
@@ -18,18 +16,40 @@ const SearchInput = ({ onInputChange }: { onInputChange: (e: any) => void }) => 
             if (searchInput !== "") {
                 onInputChange(searchInput)
             }
-        }, 2000)
+        }, 400)
 
         return () => clearTimeout(getData)
     }, [searchInput])
 
-    return <input className={style.SearchInput} placeholder="Search for a device" onChange={e => setSearchInput(e.target.value)} />
+    return <input className={style.SearchInput} placeholder="Search for a device"
+        onChange={e => setSearchInput(e.target.value)}
+    />
+}
+
+interface NodeData {
+    date: string;
+    id: string;
+    product_code: string;
+    name: string;
+    generic_name: string;
+}
+
+interface GraphData {
+    nodes: NodeData[];
+    links: any[];
 }
 
 export const DeviceGraph = () => {
-    const [graphData, setGraphData] = useState<any>({ links: [], nodes: [] });
+    const [graphData, setGraphData] = useState<GraphData & { product_descriptions: any; }>(
+        { links: [], nodes: [], product_descriptions: {} }
+    );
 
     const [selectedNode, setSelectedNode] = useState<string>("K223649");
+    const [selectedNodeData, setSelectedNodeData] = useState<NodeData | undefined>();
+
+    const [searchResults, setSearchResults] = useState<NodeData[]>([])
+
+    const [numExtraResults, setNumExtraResults] = useState(0);
 
     const data = "/fda-510k-graph/d3_fda_510k_graph.json"
 
@@ -37,47 +57,57 @@ export const DeviceGraph = () => {
         fetch("/fda-510k-graph/d3_fda_510k_graph.json").then(response => {
             return response.json();
         }).then(data => {
-            // Work with JSON data here
-            setGraphData(data);
+            // expand the generic names out from the normalized json field
+            const expandedNodeData = data.nodes.map((node) => {
+                return { ...node, generic_name: data.product_descriptions[node.product_code] }
+            })
+            console.log(expandedNodeData)
+            setGraphData({ ...data, nodes: expandedNodeData });
+            setSelectedNodeData(findNode(data, selectedNode))
         }).catch(err => {
             // Do something for an error here
             console.log("Error Reading data " + err);
         });
     }, [])
 
-    const findNode = (nodeId: string) => {
+    const findNode = (graphData: GraphData, nodeId: string) => {
         return graphData.nodes.find((node: { id: string }) => node.id === nodeId)
     }
 
-    const selectedNodeData = graphData.length ? null : findNode(selectedNode)
+    const searchForNodes = (graphData: GraphData, query: string) => {
+        return graphData.nodes.filter((node) => node.id.includes(query) || node.name.includes(query) || node.generic_name?.includes(query))
+    }
 
-    const findPredicates = (nodeId: string) => {
+
+    const findPredicates = (graphData: GraphData, nodeId: string) => {
         return graphData.links.filter((link: { source: string; target: string }) => link.target === nodeId)
     }
 
-    const findSubgraph = (graphData: { nodes: any[], links: any[] }, nodeId: string) => {
+    console.log("selected", selectedNodeData)
+
+    const findSubgraph = (graphData: GraphData, nodeId: string) => {
         const seen = new Set()
 
         const tempEdges = []
         const tempNodes = []
 
-        const device_tree = findPredicates(nodeId)
+        const device_tree = findPredicates(graphData, nodeId)
 
-        tempNodes.push(findNode(nodeId))
+        tempNodes.push(findNode(graphData, nodeId))
         seen.add(nodeId)
 
         while (device_tree.length) {
             const { target, source } = device_tree.pop()
             if (!seen.has(target)) {
-                tempNodes.push(findNode(target))
+                tempNodes.push(findNode(graphData, target))
                 seen.add(target)
             }
 
             if (!seen.has(source)) {
-                tempNodes.push(findNode(source))
+                tempNodes.push(findNode(graphData, source))
                 seen.add(source)
 
-                const sourcePredicates = findPredicates(source)
+                const sourcePredicates = findPredicates(graphData, source)
                 device_tree.push(...sourcePredicates)
                 tempEdges.push({ source, target })
             }
@@ -88,32 +118,71 @@ export const DeviceGraph = () => {
 
     console.log(graphData, selectedNode)
 
+
+
     const finalData = findSubgraph(graphData, selectedNode);
 
-    const [width, height] = useWindowSize()
+    const handleClick = useCallback(node => {
+        setSelectedNode(node.id)
+        setSelectedNodeData(findNode(graphData, node.id))
+    }, [graphData]);
 
     return <>
-        <SearchInput onInputChange={setSelectedNode}></SearchInput>
+        <SearchInput onInputChange={(e) => {
+            console.log(e)
+            const nodes = searchForNodes(graphData, e)
+
+            if (nodes.length > 10) {
+                setSearchResults(nodes.slice(0, 10))
+                setNumExtraResults(nodes.length - 10)
+            } else {
+                setSearchResults(nodes)
+            }
+
+        }}></SearchInput>
+        {searchResults.length && <div className={style.SearchResultsWrapper}>
+            <div className={style.SearchResults}>
+                {searchResults.map(node => {
+                    return <div key={node.id} className={style.SearchResult} onClick={
+                        (e: any) => {
+                            setSelectedNode(node.id)
+                            setSelectedNodeData(node)
+                            setSearchResults([])
+                            setNumExtraResults(0)
+                        }
+                    }>{node.id} - {node.name}</div>
+                })}
+                {numExtraResults > 0 &&
+                    <div className={style.MoreResults}>
+                        {numExtraResults} more results...
+                    </div>}
+            </div>
+
+        </div>}
         <div className={style.InfoSection}>
-            <span>Device ID: {selectedNode} </span>
-            <span>Date Recieved: {selectedNodeData?.date} </span>
-            <span>Product Code: <a href={
+            <p>Name: {selectedNodeData?.name}</p>
+            <p>Device ID: <a href={`https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfPMN/pmn.cfm?ID=${selectedNodeData?.id}`}
+                target="_blank" rel="noopener noreferrer">{selectedNode}</a></p>
+            <p>Date Recieved: {selectedNodeData?.date} </p>
+            <p>Product Code: <a href={
                 `https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfPCD/classification.cfm?id=${selectedNodeData?.product_code}`
-            }>{selectedNodeData?.product_code}</a> </span>
-            <span>Generic Name: {graphData?.product_descriptions?.[selectedNodeData?.product_code]}</span>
+            } target="_blank" rel="noopener noreferrer">
+                {selectedNodeData?.product_code}
+            </a>
+            </p>
+            <p>Generic Name: {graphData?.product_descriptions?.[selectedNodeData?.product_code]}</p>
         </div>
 
         <ForceGraph
             graphData={finalData}
-            nodeLabel={node => `ID: ${node.id}<br>Date: ${node.date}<br>Category: ${node.product_code}`}
+            nodeLabel={node => `Name: ${node.name}<br>ID: ${node.id}<br>Date: ${node.date}<br>Category: ${node.product_code}`}
             nodeAutoColorBy="product_code"
             linkDirectionalArrowLength={3.5}
             linkDirectionalArrowRelPos={1}
             dagMode="radialin"
-            width={width - 50}
-            height={height - 200}
             dagLevelDistance={20}
             nodeVal={(node: any) => node.id === selectedNode ? 10 : 1}
+            onNodeClick={handleClick}
             nodeCanvasObject={(node, ctx, globalScale) => {
                 const label = node.id;
                 const fontSize = 12 / globalScale;
