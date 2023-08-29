063938b4aaaa71c3880803af0ce70ab85336172b
William Edmisten <wcedmisten@gmail.com>
2023-08-29 18:35:10 -0400

    Add link to video

--- pages/talks.tsx
+++ pages/talks.tsx
@@ -7,15 +7,16 @@ function TalksPage() {
             <h1 className="post-title">Conference Talks</h1>
             <ListGroup variant="flush">
                 <PostItem
-                    href="/post/visualizing-hospital-accessibility/"
+                    href="https://www.youtube.com/watch?v=BU0oJBljc4I"
                     thumbnailURL="/og-images/north-america-hospital-distance.png"
                     thumbnailAlt="A map of North America showing the drive time to the nearest hospital."
                     title="Mapping Hospital Accessibility with OpenStreetMap (SOTMUS 2023)"
                     description={
                         <>
-                            Video not yet available.
                             Analyzing hospital accessibility using OSM and creating an interactive visualization of the data.
-                            Given at State of the Map US 2023. <a href="/slides/mapping_hospital_accessibility_slides.pdf">Slides</a>
+                            Given at State of the Map US 2023.{' '}
+                            <a href="/slides/mapping_hospital_accessibility_slides.pdf">Slides.</a>{' '}
+                            <a href="/post/visualizing-hospital-accessibility/">Original post.</a>
                         </>
                     }
                     date='2023-06-09'
