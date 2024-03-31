4948a83f61cc8781db5691a5f218466b0d470069
William Edmisten <wcedmisten@gmail.com>
2024-03-31 15:20:53 -0400

    Hospital ownership project

---
+++ pages/project/virginia-hospital-ownership/index.tsx
@@ -0,0 +1,114 @@
+import Head from "next/head"
+import { useEffect, useRef, useState } from "react"
+
+import maplibregl from "maplibre-gl";
+import * as pmtiles from "pmtiles";
+import layer from "./positron_style.json"
+import hospitals from "./hospitals_layer.json"
+import voronoi from "./voronoi.json"
+
+// const [initialLon, initialLat] = [41.1533, 20.1683]
+const [initialLon, initialLat] = [-76.5059133431, 37.6227077717]
+
+const Map = () => {
+  const mapContainer = useRef(null);
+  const map = useRef<maplibregl.Map>();
+
+  const popup = useRef<maplibregl.Popup>();
+
+  const [lng] = useState(initialLon);
+  const [lat] = useState(initialLat);
+
+  const [zoom] = useState(4);
+
+  useEffect(() => {
+    if (map.current) return;
+
+    let protocol = new pmtiles.Protocol();
+    maplibregl.addProtocol("pmtiles", protocol.tile);
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current as any,
+      style: {
+        glyphs: "/virginia-hospital-distance/glyphs/{fontstack}/{range}.pbf",
+        version: 8,
+        "sources": {
+          "openmaptiles": {
+            "type": "vector",
+            "url": "pmtiles:///virginia-hospital-distance/virginia.pmtiles"
+          },
+          "hospitals": {
+            "type": "geojson",
+            "data": hospitals
+          },
+          "voronoi": {
+            "type": "geojson",
+            "data": voronoi
+          }
+        },
+        layers: layer as any,
+      },
+      center: [lng, lat],
+      zoom: zoom,
+      minZoom: 4,
+      // maxBounds: [
+      //   [-84.71490710282056,
+      //     35.77320086387027],
+      //   [-73.94080914265395,
+      //     39.73148308878754]],
+      customAttribution: ["© OpenMapTiles", "© OpenStreetMap contributors"],
+    });
+
+    if (!popup.current) {
+      // Create a popup, but don't add it to the map yet.
+      popup.current = new maplibregl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+    }
+
+    map.current.loadImage(
+      '/virginia-hospital-distance/hospital-solid.png',
+      // Add an image to use as a custom marker
+      function (error, image: any) {
+        if (error) throw error;
+        map.current?.addImage('hospitalMarker', image);
+      })
+  });
+
+  return (
+    <div className="map-wrap">
+      <div ref={mapContainer} className="map" />
+    </div>
+  );
+}
+
+
+export default function Isochrone() {
+  return (
+    <>
+      <Head>
+        <title>Virginia Hospital Accessibility Map</title>
+        <meta name="description" content="A map visualizing the travel time by car to the nearest hospital in Virginia." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Map></Map>
+      </main>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      opengraph: {
+        title: "Drive Time to Nearest Hospital in Virginia",
+        image: "https://wcedmisten.fyi/og-images/virginia-hospital-distance.png",
+        type: "website",
+        url: "https://wcedmisten.fyi/project/virginia-hospital-distance"
+      }
+    }
+  }
+}
